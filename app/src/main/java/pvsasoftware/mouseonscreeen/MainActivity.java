2f5e99aa393fda8985cf4a9ce943494aa5609419
sonthanh297 <thienthan297@gmail.com>
2017-04-19 00:11:43 +0700

    đ
    dd

--- app/src/main/java/pvsasoftware/mouseonscreeen/MainActivity.java
+++ app/src/main/java/pvsasoftware/mouseonscreeen/MainActivity.java
@@ -1,7 +1,17 @@
-package pvsasoftware.mouseonscreeen;
 
+package pvsasoftware.myapplication;
+
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Build.VERSION.SDK_INT >= 23 && (!Settings.canDrawOverlays(getApplicationContext()))) {
+                    startActivityForResult(new Intent("android.settings.action.MANAGE_OVERLAY_PERMISSION", Uri.parse("package:"+ getPackageName())),100);
+                } else {
+                    MainActivity.this.startService(new Intent(getApplicationContext(),Myservice.class));
+                    Intent newIntent = new Intent("android.intent.action.MAIN");
+                    newIntent.addCategory("android.intent.category.HOME");
+                    startActivity(newIntent);
+                }
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (Build.VERSION.SDK_INT >= 23 && Settings.canDrawOverlays(this)) {
+                MainActivity.this.startService(new Intent(getApplicationContext(),Myservice.class));
+                Intent newIntent = new Intent("android.intent.action.MAIN");
+                newIntent.addCategory("android.intent.category.HOME");
+                startActivity(newIntent);
+            }
+        } else {
+            return;
+        }
     }
 }
--- app/src/main/java/pvsasoftware/mouseonscreeen/Myservice.java
+++ app/src/main/java/pvsasoftware/mouseonscreeen/Myservice.java
@@ -1,23 +1,26 @@
 package pvsasoftware.mouseonscreeen;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.Point;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
 import java.lang.reflect.Field;
-
-/**
- * Created by thien on 4/14/2017.
- */
-
+import java.util.Random;
 public class Myservice extends Service {
-
 
     private int device_height;
     private int device_width;
@@ -31,7 +34,7 @@ public class Myservice extends Service {
     private WindowManager windowManager;
     private long mouse_duration;
     private int mouse_speed;
-
+    private TranslateAnimation animation;
 
     private static WindowManager.LayoutParams createDefaultSystemWindownParams(boolean paramBoolean) {
         for (int i = 256;; i = 0) {
@@ -40,8 +43,6 @@ public class Myservice extends Service {
             return localLayoutParams;
         }
     }
-
-
 
 
     @Nullable
@@ -55,24 +56,26 @@ public class Myservice extends Service {
         super.onCreate();
         this.windowManager = (WindowManager)getSystemService(WINDOW_SERVICE);
         Display localDisplay = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+        this.mRootView = LayoutInflater.from(this).inflate(R.layout.home,null,false);
+        this.mDesignMouse_top_right = (ImageView)mRootView.findViewById(R.id.design_mouse_top_right);
+        this.mDesignMouseFunny_Bottom = (ImageView)mRootView.findViewById(R.id.funny_mouse_bottom);
         Point point = new Point();
         localDisplay.getSize(point);
         this.device_height = point.x;
         this.device_width = point.y;
         this.drawableArray = new int[40];
-        int i = 0;
-        for (;;) {
+        for (int i = 0;i < 40; i++) {
             if ( i< 40) {
                 try {
-                    this.drawableArray[i] = getID("imgg_" + String.format("%04d",new Object[]{Integer.valueOf(i+1)}),R.drawable.class);
+                    this.drawableArray[i] = getID("imgg_" + String.format("%04d",new Object[]{Integer.valueOf(i+1)}), android.R.drawable.class);
                 } catch (Exception e) {
-                    for (;;) {
-                        e.printStackTrace();
-                    }
+
+                    e.printStackTrace();
+
                 }
             }
         }
-
+        startMouse();
 
     }
 
@@ -86,8 +89,114 @@ public class Myservice extends Service {
         }
     }
 
+    private void startMouse() {
+        setOverLay();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setOverLay2();
+            }
+        },8000);
+        this.windowManager.addView(mRootView,createDefaultSystemWindownParams(true));
+    }
 
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return Service.START_NOT_STICKY;
+    }
 
+    public void setOverLay() {
+        BitmapDrawable bd = (BitmapDrawable)getResources().getDrawable(R.drawable.r1_imgg_0004);
+        final int img_height = bd.getBitmap().getHeight();
+        final int img_width = bd.getBitmap().getWidth();
+        int[] resArray = new int[40];
+        for (int i = 0;i<40;i++) {
+            try {
+                resArray[i] = getID("r1_imgg_" + String.format("%04d",new Object[]{Integer.valueOf(i+1)}),R.drawable.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
+        AnimationDrawable animationDrawable = new AnimationDrawable();
+        for (int j = 0; j< 40;j++) {
+            animationDrawable.addFrame(getResources().getDrawable(resArray[j]),80);
+        }
+        animationDrawable.setOneShot(false);
+        this.mDesignMouse_top_right.setImageDrawable(animationDrawable);
+        animationDrawable.start();
+        animation = new TranslateAnimation(0.0f,0.0f,(float)(-img_height),this.device_height);
+        this.animation.setDuration(8000);
+        this.mDesignMouse_top_right.startAnimation(animation);
+        this.animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+//                int i = device_width;
+//                int j = img_height/2;
+//                 i = new Random().nextInt(((i-j-0 +1 ) + 0));
+//                mDesignMouse_top_right.layout(i,-j,mDesignMouse_top_right.getMeasuredWidth()+i,j+mDesignMouse_top_right.getMeasuredHeight());
+                // mDesignMouse_top_right.setX(device_height/3);
+                //mDesignMouse_top_right.setY(device_height/3);
+                mDesignMouse_top_right.startAnimation(animation);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+    public void setOverLay2() {
+        BitmapDrawable bd = (BitmapDrawable)getResources().getDrawable(R.drawable.r1_imgg_0004);
+        final int img_height = bd.getBitmap().getHeight();
+        final int img_width = bd.getBitmap().getWidth();
+        int[] resArray = new int[40];
+        for (int i = 0;i<40;i++) {
+            try {
+                resArray[i] = getID("r1_imgg_" + String.format("%04d",new Object[]{Integer.valueOf(i+1)}),R.drawable.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        AnimationDrawable animationDrawable = new AnimationDrawable();
+        for (int j = 0; j< 40;j++) {
+            animationDrawable.addFrame(getResources().getDrawable(resArray[j]),80);
+        }
+        animationDrawable.setOneShot(false);
+        this.mDesignMouseFunny_Bottom.setImageDrawable(animationDrawable);
+        animationDrawable.start();
+        animation = new TranslateAnimation((float)-img_width,this.device_width,0.0f,0.0f);
+        this.animation.setDuration(10000);
+        this.mDesignMouseFunny_Bottom.startAnimation(animation);
+        this.animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+//                int i = device_width;
+//                int j = img_height/2;
+//                 i = new Random().nextInt(((i-j-0 +1 ) + 0));
+//                mDesignMouse_top_right.layout(i,-j,mDesignMouse_top_right.getMeasuredWidth()+i,j+mDesignMouse_top_right.getMeasuredHeight());
+                // mDesignMouse_top_right.setX(device_height/3);
+                //mDesignMouse_top_right.setY(device_height/3);
+                mDesignMouseFunny_Bottom.startAnimation(animation);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
 
 }
